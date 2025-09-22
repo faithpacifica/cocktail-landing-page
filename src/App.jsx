72eb25b488fad4e084f9bf712f8aa9f00d330dd9
Dildora Muslimova <faithpacifica@yahoo.com>
2025-09-22 22:43:50 +0900

    create all components skeleton

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
+import Navbar from './components/Navbar';
+import Hero from "./components/Hero";
+import Cocktails from "./components/Cocktails";
+import About from "./components/About";
+import Art from "./components/Art";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 gsap.registerPlugin(ScrollTrigger, SplitText);
-// import './index.css';
+
 
 function App() {
   return (
-    <>
-      <main className="text-amber-500 text-5xl h-[100vh]">
-        COCKTAIL PAGE
-      </main>
-    </>
+    <main >
+      <Navbar />
+      <Hero />
+      <Cocktails />
+      <About />
+      <Art />
+      <Menu />
+      <Contact/>
+    </main>
   );
 }
 
