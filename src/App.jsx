2345cb8f4806bc39f802b5aa24b3eb6831fbc277
Dildora Muslimova <faithpacifica@yahoo.com>
2025-09-22 22:24:31 +0900

    add all styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { ScrollTrigger, SplitText} from "gsap/all";
+import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 gsap.registerPlugin(ScrollTrigger, SplitText);
+// import './index.css';
 
 function App() {
   return (
     <>
-      <main className="text-amber-500 font-extrabold">COCKTAIL PAGE</main>
+      <main className="text-amber-500 text-5xl h-[100vh]">
+        COCKTAIL PAGE
+      </main>
     </>
   );
 }
